952efcb093b1725a926eef16cfe17691fd891130
Emerson <emerson.ramos.dos.santos2006@gmail.com>
2025-04-23 18:10:04 -0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -27,14 +27,14 @@ const Header = () => {
         <div className='flex justify-between items-center'>
             <span className='font-medium text-2xl sm:text-3xl'>Posts</span>
            
-            <button className='py-3 px-4 sm:py-4 sm:px-7 bg-blue-100 text-blue-500 rounded-full cursor-pointer font-semibold text-center transition-all duration-500 hover:bg-blue-500 hover:text-white text-base'>
-              Register Now
+            <button className='hover:translate-x-px transition duration-400 py-3 px-4 sm:py-4 sm:px-7 bg-blue-100 text-blue-500 rounded-full cursor-pointer font-semibold text-center transition-all duration-500 hover:bg-blue-500 hover:text-white text-base'>
+              Follow me!
           </button>
 
         </div>
         <div className='text-center my-8'>
-            <h1 className='text-3xl sm:text-5xl font-medium'>My Posts</h1>
-            <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-lg'>Whether you're passionate about coding, design, or just curious to learn something new, this is the place for you. Let’s explore, create, and innovate together!</p>
+            <h1 className='text-3xl sm:text-5xl font-medium'>Hi 👋! What's up?</h1>
+            <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-lg'>My name is Emerson, I love UI/UX and Frontend, but I also understand backend. So... that makes me a fullstack. In this blog, I talk about technology, but also about other types of topics, like more philosophical subjects and such.</p>
             <form onSubmit={onSubmitHandler} className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 gap-4'>
                 <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" placeholder='Enter your better email' className='pl-4 outline-none w-full border-b border-black' required />
                 <button type='submit' className='border border-zinc-900 py-4 px-4 sm:px-4 duration-100 active:bg-green-500 active:border-green-500 active:text-white rounded-lg'>Subscribe</button>
