a904665cdf6d23c921b87753c334fea29cc7d62e
Emerson <emerson.ramos.dos.santos2006@gmail.com>
2025-04-24 09:46:40 -0300

    Update BlogList.jsx

--- components/BlogList.jsx
+++ components/BlogList.jsx
@@ -21,10 +21,14 @@ const BlogList = () => {
   return (
     <div>
         <div className='flex justify-center gap-6 sm:gap-8 my-10'>
-            <button onClick={()=> setMenu('All')} className={menu==="All" ? 'text-blue-700 rounded-full bg-blue-100 px-2 py-1.5':""}>All</button>
-            <button onClick={()=> setMenu('Technology')} className={menu==="Technology" ? 'text-blue-700 rounded-full px-2 py-1.5 bg-blue-100':""}>Technology</button>
-            <button onClick={()=> setMenu('Startup')} className={menu==="Startup" ? 'text-blue-700 rounded-full px-2 py-1.5 bg-blue-100':""}>Startup</button>
-            <button onClick={()=> setMenu('Lifestyle')} className={menu==="Lifestyle" ? 'text-blue-700 rounded-full px-2 py-1.5 bg-blue-100':""}>Lifestyle</button>
+
+            <button onClick={()=> setMenu('All')} className={menu==="All" ? 'text-blue-700 rounded-full bg-blue-100 px-3 py-1':""}>All</button>
+
+            <button onClick={()=> setMenu('Technology')} className={menu==="Technology" ? 'text-blue-700 rounded-full px-3 py-1 bg-blue-100':""}>Technology</button>
+
+            <button onClick={()=> setMenu('Thought')} className={menu==="Thought" ? 'text-blue-700 rounded-full px-3 py-1 bg-blue-100':""}>Thought</button>
+
+            <button onClick={()=> setMenu('Lifestyle')} className={menu==="Lifestyle" ? 'text-blue-700 rounded-full px-3 py-1 bg-blue-100':""}>Lifestyle</button>
         </div>
         <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
             {blogs.filter((item)=> menu==="All"?true:item.category===menu ).map((item, index) => {
